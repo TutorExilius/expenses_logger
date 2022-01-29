592179e1f4a67e3b8d99d951e6371821e8f81486
Tutor Exilius <tutorexilius@gmail.com>
2022-01-29 23:25:58 +0100

    feat: save projects working dir in globals.py

--- expenses_logger/globals.py
+++ expenses_logger/globals.py
@@ -1,5 +1,8 @@
-from expenses_logger.helper import get_app_major_version
+from pathlib import Path
 
+from expenses_logger.logic.helper import get_app_major_version
+
+WORKING_DIR = Path(__file__).parent.parent
 DATABASE_FILE = f"db_v{get_app_major_version()}.X.sqlite3"
 SYNC_NAS_DIR = "/home/pi/myNAS/myShare/"
 SYNC_NAS_DIR = "c:/tmp/"
