337f35a9100466c6b189215d42f8cddeb3f564f9
Tutor Exilius <tutorexilius@gmail.com>
2022-02-09 22:05:57 +0100

    feat: coroutine support for qt with asyncqt

--- expenses_logger/main.py
+++ expenses_logger/main.py
@@ -1,4 +1,5 @@
 import sys
+import asyncio
 from expenses_logger.globals import USERS
 
 from asyncqt import QEventLoop
@@ -10,12 +11,18 @@ from expenses_logger.view.main_wizard import MainWizard
 
 def main() -> None:
     app = QApplication(sys.argv)
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
 
     mainWindow = MainWizard(user_names=USERS, parent=None, desktop_size=None)
+    screenrect = app.primaryScreen().geometry()
 
+    mainWindow.move(screenrect.left(), screenrect.top())
+    mainWindow.setFixedSize(QSize(800, 480))
     mainWindow.show()
 
-    app.exec()
+    with loop:
+        sys.exit(loop.run_forever())
 
 
 if __name__ == "__main__":
